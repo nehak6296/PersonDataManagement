1adc0ea96c1d8a46e4f4e1bb4a825fee2c11032f
nehak6296 <nehak6296@gmail.com>
2021-03-19 13:53:08 +0530

    [Neha]Added:Person class and List

--- PersonDataManagement/PersonDataManagement/Program.cs
+++ PersonDataManagement/PersonDataManagement/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PersonDataManagement
 {
@@ -6,7 +7,21 @@ namespace PersonDataManagement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Person Data Management!!");
+            //UC-1
+            List<Person> listPersonInCity = new List<Person>();
+            AddRecords(listPersonInCity);
+        }
+        private static void AddRecords(List<Person> listPersonInCity)
+        {
+            listPersonInCity.Add(new Person("203456876", "John", "12 Main Street, Newyork,NY", 15));
+            listPersonInCity.Add(new Person("203456877", "SAM", "13 Main Ct, Newyork,NY", 25));
+            listPersonInCity.Add(new Person("203456878", "Elan", "14 Main Street, Newyork,NY", 35));
+            listPersonInCity.Add(new Person("203456879", "Smith", "12 Main Street, Newyork,NY", 45));
+            listPersonInCity.Add(new Person("203456880", "SAM", "345 Main Ave, Dayton,OH", 55));
+            listPersonInCity.Add(new Person("203456881", "Sue", "32 Cranbrook Rd, Newyork,NY", 65));
+            listPersonInCity.Add(new Person("203456882", "Winston", "1208 Alex st, Newyork,NY", 65));
+            listPersonInCity.Add(new Person("203456883", "Mac", "126 Province Ave, Baltimore,NY", 85));
+            listPersonInCity.Add(new Person("203456884", "SAM", "126 Province Ave, Baltimore,NY", 95));
         }
     }
 }
